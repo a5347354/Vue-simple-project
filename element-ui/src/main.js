24ad038dc1eeed7ac77343f63de275d04500c982
LukeFan <a5347354@gmail.com>
2020-02-24 22:37:17 +0800

    Change i18N to zh-TW

--- element-ui/src/main.js
+++ element-ui/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/zh-TW';
 import router from './router';
 import store from './store';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 Vue.config.productionTip = false;
 
 
