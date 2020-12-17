706641fcc1e9e71f083a2c970ca49c75c0883c6a
LukeFan <a5347354@gmail.com>
2020-12-17 09:07:13 +0800

    Changing eslint rules and install sass

--- element-ui/.eslintrc.js
+++ element-ui/.eslintrc.js
@@ -13,5 +13,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
   },
 };
