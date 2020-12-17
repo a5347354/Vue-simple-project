c1b7155f37c87deaf48eafead111ac690995ca7b
LukeFan <a5347354@gmail.com>
2020-12-17 09:07:31 +0800

    Adding Circle Progree Bar

--- element-ui/src/views/Home.vue
+++ element-ui/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Table />
+    <CircleProgressBar />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Table from '@/components/Table/index.vue';
+import CircleProgressBar from '@/components/CircleProgressBar/index.vue';
 
 export default {
   name: 'Home',
   components: {
     Table,
+    CircleProgressBar,
   },
 };
 </script>
